2474aa6589b01e0d6efa57685b7e0b17f4209cfb
Francesco Urso <f.urso3@studenti.unipi.it>
2022-11-09 10:12:00 +0100

    Add files via upload

---
+++ DatiRX/ripr.py
@@ -0,0 +1,19 @@
+import numpy  as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+Path.cwd()
+data=np.loadtxt('Riproducibilità.txt',skiprows=6)
+
+D_aria, counts=np.unique(data, return_counts=True)
+
+
+plt.figure(1)
+
+plt.scatter(D_aria,counts)
+plt.figure(2)
+#oppure istogramma
+
+plt.hist(data,bins=18)
+plt.show()
+
+
