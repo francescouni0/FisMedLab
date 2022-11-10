b04f6b5857cabdf32f759207361b759ce2d10092
simonedamiani <114066103+simonedamiani@users.noreply.github.com>
2022-11-10 16:12:43 +0100

    Update D_vs_I.py

--- DatiRX/D_vs_I.py
+++ DatiRX/D_vs_I.py
@@ -1,6 +1,12 @@
 import numpy as np
+from scipy import optimize
 import matplotlib.pyplot as plt
 from pathlib import Path
+
+
+def fit_func(x, m, b):
+    return m*x+b
+
 
 Path.cwd()
 
@@ -8,7 +14,13 @@ data = np.loadtxt('D_vs_I.txt', skiprows=8)
 print(np.shape(data))
 I = data[:, 0]
 Dose = data[:, 1]
+Dd = Dose*0.01
+print(Dose.shape)
+print(I.shape)
 
+params, params_cov = optimize.curve_fit(fit_func, I, Dose, sigma=Dd)
+print(params)
+'''
 coeff = np.polyfit(I, Dose, 1)
 print(f'y=A*x+B \nA: {coeff[0]} \nB: {coeff[1]}')
 chi_squared = np.sum((np.polyval(coeff, I) - Dose) ** 2)
@@ -19,6 +31,10 @@ print(f'Chi Squared: {chi_squared} \nDegree of freedom: {dof} \nChi Reduced: {ch
 xn = np.linspace(min(I), max(I), 100)
 yn = np.poly1d(coeff)
 plt.plot(xn, yn(xn), '-r', I, Dose, 'og')
+'''
+plt.errorbar()
+plt.errorbar(I, fit_func(I, *params), I, Dose, yerr=Dd)
+
 plt.title('Dose vs Tube Current')
 plt.xlabel('Current[mA]')
 plt.ylabel('Dose[uGy]')
