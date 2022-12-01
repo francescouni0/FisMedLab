8eb8ffe93abbc5441b4719dce1bdf63c2c7adf55
simonedamiani <114066103+simonedamiani@users.noreply.github.com>
2022-12-01 20:14:37 +0100

    Simo commit

--- Silici/Cal_segnali/calibrazione_segnali.m
+++ Silici/Cal_segnali/calibrazione_segnali.m
@@ -1,8 +1,14 @@
+%% Pulisci tutto
 clear
 close all
 
+%% Calcola media e deviazione standard di ogni picco
 mean = [];
 sigma = [];
+
+[m, s] = peak_analysis("3.04.Spe");
+mean = [mean m];
+sigma = [sigma s];
 
 [m, s] = peak_analysis("2.80.Spe");
 mean = [mean m];
@@ -23,6 +29,13 @@ sigma = [sigma s];
 [m, s] = peak_analysis("0.88.Spe");
 mean = [mean m];
 sigma = [sigma s];
+
+%% Fit/calibrazione
+e = 1.602e-19;
+E_gap = 3.9;
+C = 1e-12;
+
+signal = []
 
 weight = 1./sqrt(sigma);
 
