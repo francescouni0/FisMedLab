4a7a66151a1281640209bc255bbdf3fbb6da54fe
francescouni0 <f.urso3@studenti.unipi.it>
2022-12-02 10:18:19 +0100

    AGGIUNTO ALGORITMO PER SOTTRARE IL CONTINUM, ESCE BENE C'È DA STUDIARE PER GIUSTIFICARLO

--- Spettroscopiagamma/RisE.py
+++ Spettroscopiagamma/RisE.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 from scipy.signal import find_peaks
 from scipy.optimize import curve_fit
+from scipy import sparse
+from scipy.sparse.linalg import spsolve
 
 Path.cwd()
 
@@ -40,7 +42,7 @@ y=[662,31,81,356,60,1174,1332,511,1274]
 
 #ALGORITMO PER ELIMINARE IL CONTINUM
 
-x=x[590:710]
+c=x[590:710]
 a=[]
 b=[]
 a=np.append(a,Na22cal[460:490])
@@ -51,18 +53,77 @@ Mi=np.sum(a)/len(a)
 Mf=np.sum(b)/len(b)
 F=((Mi+Mf)*440)/2
 
-Na22net=np.sum(Na22cal[x])-F
-Na22gross=np.sum(Na22cal[x])
+Na22net=np.sum(Na22cal[c])-F
+Na22gross=np.sum(Na22cal[c])
 print(Na22net)
 print(Na22gross)
+
+
+
+#MULTILEASTSQUARED ESKERE
+
+
+
+def baseline_als(y, lam, p, niter=10):
+  L = len(y)
+  D = sparse.diags([1,-2,1],[0,-1,-2], shape=(L,L-2))
+  w = np.ones(L)
+  for i in range(niter):
+    W = sparse.spdiags(w, 0, L, L)
+    Z = W + lam * D.dot(D.transpose())
+    z = spsolve(Z, w*y)
+    w = p * (y > z) + (1-p) * (y < z)
+  return z
+
+Na22base=baseline_als(Na22cal, 1e5, 0.001)
+print(Na22base)
+Co60base=baseline_als(Co60cal, 1e5, 0.001)
+Ba133base=baseline_als(Ba133cal, 1e5, 0.001)
+Am241base=baseline_als(Am241cal, 1e5, 0.001)
+Cs137base=baseline_als(Cs137cal, 1e5, 0.001)
+
+
+Na22net1=np.sum(Na22cal[c]-Na22base[c])
+print(Na22net1)
+'''
+Co60net2=np.sum(Co60cal[c]-Co60base[c])
+Ba133net3=np.sum(Ba133cal[c]-Ba133base[c])
+Cs137net4=np.sum(Cs137cal[c]-Cs137base[c])
+Am241net5=np.sum(Am241cal[c]-Am241base[c])
+'''
 #PLOTTO
 
 
-plt.plot(x,Na22cal[x])
-#plt.scatter(pNa,Na22clean[pNa])
-plt.title('Na22')
+
+fig, axs = plt.subplots(2, 2)
+axs[0, 0].set_title('Cs137')
+axs[0,0].plot(x,Cs137cal)
+axs[0,0].plot(x,Cs137cal-Cs137base,'tab:orange')
+
+axs[0, 1].plot(x,Ba133cal-Ba133base,'-m')
+axs[0,1].plot(x,Ba133cal)
+
+axs[0, 1].set_title('Ba133')
+axs[1, 0].plot(x,Am241cal-Am241base,'tab:red')
+axs[1,0].plot(x,Am241cal)
+
+axs[1, 0].set_title('Am241')
+axs[1, 1].plot(x,Co60cal-Co60base,'tab:green')
+axs[1,1].plot(x,Co60cal)
+
+axs[1, 1].set_title('Co60')
+
+for ax in axs.flat:
+    ax.set(xlabel='x-label', ylabel='y-label')
+
+
 plt.show()
 
+
+plt.plot(x,Na22cal-Na22base,'tab:pink')
+plt.plot(x,Na22cal)
+plt.title('Na22')
+plt.show()
 
 
 
