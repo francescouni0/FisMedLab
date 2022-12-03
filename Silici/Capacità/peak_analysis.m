22d14f70518bfee21fef2f825f3f4aad98f4e626
simonedamiani <114066103+simonedamiani@users.noreply.github.com>
2022-12-04 00:47:02 +0100

    Capacità

---
+++ Silici/Capacità/peak_analysis.m
@@ -0,0 +1,14 @@
+function [mean, sigma] = peak_analysis(file_name)
+
+data = table2array(importfile_spe(file_name));
+channel = transpose(linspace(1, length(data), length(data)));
+
+peakfit = fit(channel, data, 'gauss1', 'Exclude', channel < 50);
+
+mean = peakfit.b1;
+sigma = peakfit.c1;
+
+%plot(peakfit,channel,data)
+%xlim([0 500])
+
+end
