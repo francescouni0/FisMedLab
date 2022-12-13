0a99e93cf4eb452fffec201dae7db73c6002a793
simonedamiani <114066103+simonedamiani@users.noreply.github.com>
2022-12-13 16:35:33 +0100

    Background

---
+++ Spettroscopiagamma/peak_analysis_background.m
@@ -0,0 +1,21 @@
+function [mean, sigma] = peak_analysis_background(spectrum, x1, x2)
+xy_A = [x1 spectrum(x1)];
+xy_B = [x2 spectrum(x2)];
+
+m = (xy_B(2)-xy_A(2))/(xy_B(1)-xy_A(1));
+k = xy_A(2)-m*xy_A(1);
+peak = [];
+index = [];
+for i = xy_A(1):xy_B(1)
+    point = m*round(i)+k;
+    peak = [peak (spectrum(round(i))-point)];
+    index = [index round(i)];
+end
+
+peakfit = fit(transpose(index), transpose(peak), 'gauss1');
+% figure
+% plot(peakfit, index, peak)
+
+mean = peakfit.b1;
+sigma = peakfit.c1;
+end
