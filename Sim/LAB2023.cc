b9be05a5f5f90520e2cf433becdc5e379ba6d80f
francescouni0 <f.urso3@studenti.unipi.it>
2023-05-06 12:14:16 +0200

    10000 eventi selezionati basta far partire run

--- Sim/LAB2023.cc
+++ Sim/LAB2023.cc
@@ -75,9 +75,9 @@ int main(int argc, char** argv) {
   // Choose the Random engine
   G4Random::setTheEngine(new CLHEP::RanecuEngine);
 
-  //G4RunManager * runManager = new G4RunManager;
+  G4RunManager * runManager = new G4RunManager;
   
-  G4MTRunManager * runManager = new G4MTRunManager;
+  //G4MTRunManager * runManager = new G4MTRunManager;
   runManager->SetNumberOfThreads(nThreads);
   
   
@@ -143,7 +143,7 @@ int main(int argc, char** argv) {
     info->greaseToSiPMFinish = "polished";    
     
     // Primary beam settings
-    info->numOfEvents         = 1000;                          // number of primary particles to generate
+    info->numOfEvents         = 10000;                          // number of primary particles to generate
  
  
     // ****************************************************************
