1549433526389065b2a08aa1fccc5497883952c1
francescouni0 <f.urso3@studenti.unipi.it>
2023-05-06 11:53:52 +0200

    Di nuovo a singolo thread

--- Sim/LAB2023.cc
+++ Sim/LAB2023.cc
@@ -56,7 +56,7 @@ int main(int argc, char** argv) {
   G4String macro;
   G4String session;
 
-  G4int nThreads = 4;
+  G4int nThreads = 2;
 
   for ( G4int i=1; i<argc; i=i+2 ) {
     if      ( G4String(argv[i]) == "-m" ) macro   = argv[i+1];
@@ -75,9 +75,9 @@ int main(int argc, char** argv) {
   // Choose the Random engine
   G4Random::setTheEngine(new CLHEP::RanecuEngine);
 
-  G4RunManager * runManager = new G4RunManager;
+  //G4RunManager * runManager = new G4RunManager;
   
-  //G4MTRunManager * runManager = new G4MTRunManager;
+  G4MTRunManager * runManager = new G4MTRunManager;
   runManager->SetNumberOfThreads(nThreads);
   
   
@@ -143,7 +143,7 @@ int main(int argc, char** argv) {
     info->greaseToSiPMFinish = "polished";    
     
     // Primary beam settings
-    info->numOfEvents         = 10000;                          // number of primary particles to generate
+    info->numOfEvents         = 1000;                          // number of primary particles to generate
  
  
     // ****************************************************************
